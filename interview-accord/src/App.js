8c15524a32591d7442a0aee1552b447224ba3b76
Saliha <salihaoguz2011@gmail.com>
2023-02-25 08:04:56 +0000

    started

---
+++ interview-accord/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="header">
+        <h2>REACT Interview Ouestions And Answers</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
