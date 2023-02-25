f0b13c4fa7c9e8fc3f6d45d902473346dcf38e1f
Saliha <salihaoguz2011@gmail.com>
2023-02-25 08:23:44 +0000

    components

--- interview-accord/src/App.js
+++ interview-accord/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import "./App.css";
+import InterviewAccord from "./components/InterviewAccord";
+import data from "./helper/data";
 
 function App() {
   return (
     <>
       <div className="header">
         <h2>REACT Interview Ouestions And Answers</h2>
+      </div>
+      <div className="app">
+        <div className="main">
+          <InterviewAccord data={data} />
+        </div>
       </div>
     </>
   );
